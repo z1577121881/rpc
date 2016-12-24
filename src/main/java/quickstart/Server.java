bf4071ec893f4f1ea4342dc3e157d6362b2e5987
zyl <1577121881@qq.com>
2016-12-24 15:08:59 +0800

    结合了Consul作为注册中心的使用

--- src/main/java/quickstart/Server.java
+++ src/main/java/quickstart/Server.java
@@ -1,5 +1,7 @@
 package quickstart;
 
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,6 +11,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Server {
     public static void main(String[] args) throws InterruptedException {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:conf/motan_server.xml");
+        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
         System.out.println("server start...");
     }
 }
